da6f04c0b263ad77adad181447be28639036f379
AhmadNurHidayatullah <ahmadnurhidayatulla@gmail.com>
2025-01-13 09:13:35 +0700

    Tambah data class MahasiswaEvent

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.firebase.ui.viewmodel
+
+import com.example.firebase.model.Mahasiswa
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String= "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
